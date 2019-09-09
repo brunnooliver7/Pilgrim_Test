b0c2843af771e8182c19077360720f4454ac464d
Bruno <brunno.oliver7@gmail.com>
2019-09-09 13:21:36 -0300

    Modelando contributors

--- contributors/models.py
+++ contributors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Contributor(models.Model):
+    name = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
+    author = models.CharField(max_length=200)
+    editor = models.CharField(max_length=200)
+    reviewer = models.CharField(max_length=200)
+    email = models.EmailField()
+    country = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
+
+    def __str__(self):
+        return self.name
