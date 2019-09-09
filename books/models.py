8dd60b1fe73dba700ec42c30f791fb87a3db4a78
Bruno <brunno.oliver7@gmail.com>
2019-09-09 13:23:30 -0300

    Modelando books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from contributors.models import Contributor
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
+    language = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
